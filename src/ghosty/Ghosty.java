afcf8edec8b10fdfc9dd73baceb93f2c40bd4dba
aoshun <aoshun.alphameteor@hotmail.fr>
2012-12-27 21:58:00 +0100

    mettons le contenu comme tu l'as dit

--- src/ghosty/Ghosty.java
+++ src/ghosty/Ghosty.java
@@ -1,6 +1,4 @@
 package ghosty;
-
-
 //it's just what we can use in java.io.*
 import java.io.Reader;
 import java.io.Writer;
@@ -8,7 +6,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 
+public class Ghosty {
 
-public class XMLFileTree {
-
+	
+	
+	public static void main (String args){
+		
+		
+	}
 }
