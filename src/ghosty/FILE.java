1e96fd69519d9b0cb1e169e1f1989fedbefb42e0
aoshun <aoshun.alphameteor@hotmail.fr>
2012-12-27 23:44:11 +0100

    la classe FILE commence!!

---
+++ src/ghosty/FILE.java
@@ -0,0 +1,26 @@
+package ghosty;
+
+import java.nio.file.Path;
+
+public class FILE {
+ private Path location;
+ /**
+  * if it is a file, this number is 0 
+  * else it represent the number of element
+  */
+ private int numberelt;
+ 
+  public FILE(Path location, int numberelt){
+	  this.location=location;
+	  this.numberelt=numberelt;
+  }
+  
+  public Path getLocation(){
+	  return location;
+  }
+  
+  public int getNumberelt(){
+	  return numberelt;
+  }
+ 
+}
