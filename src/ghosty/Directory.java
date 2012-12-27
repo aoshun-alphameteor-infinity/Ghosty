e1bdbcf72a5ab6f34ac515a435fbc6a42b47c358
aoshun <aoshun.alphameteor@hotmail.fr>
2012-12-28 00:19:54 +0100

    preparatif pour demain

--- src/ghosty/Directory.java
+++ src/ghosty/Directory.java
@@ -1,5 +1,15 @@
 package ghosty;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Directory {
 
+	
+	public List<Path> fileTreeMaker(Path start){
+		List<Path> result=new ArrayList();
+		
+		return result;
+	}
 }
