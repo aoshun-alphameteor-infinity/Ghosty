f2810733b40a64287e2b778f9c396c34f520bb45
aoshun <aoshun.alphameteor@hotmail.fr>
2012-12-27 21:44:11 +0100

    juste pour mettre le fichier build

---
+++ src/ghosty/XMLFileTree.java
@@ -0,0 +1,14 @@
+package ghosty;
+
+
+//it's just what we can use in java.io.*
+import java.io.Reader;
+import java.io.Writer;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+
+public class XMLFileTree {
+
+}
