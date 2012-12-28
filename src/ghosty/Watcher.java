880293ecd609a9d523374b9c4e6beb7c2bae7e6d
karineS <karine.siette@laposte.net>
2012-12-29 00:26:56 +0100

    Update src/ghosty/Watcher.java

--- src/ghosty/Watcher.java
+++ src/ghosty/Watcher.java
@@ -1,5 +1,61 @@
 package ghosty;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.Iterator;
+
 public class Watcher {
+  private Directory dir;
+	
+	
+	public Watcher(Directory dir) {
+		this.dir=dir;
+	}
+
+	public void watch(Iterator<FILE> it){
+		try{
+			WatchService w=FileSystems.getDefault().newWatchService();
+	
+	if(!it.hasNext()) return;
+	FILE f=it.next();
+	f.getLocation().register(w, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.OVERFLOW, StandardWatchEventKinds.ENTRY_DELETE);
+	WatchKey k = w.take();
+	for (WatchEvent<?> object : k.pollEvents()) {
+        if (object.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+        	System.out.println("Modify: " + object.context().toString());
+            if(f.getNumberelt()!=0)
+            watch(it);
+        }
+        if (object.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+            System.out.println("Delete: " + object.context().toString());
+        }
+        if (object.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+            System.out.println("Created: " + object.context().toString());
+        }
+        if (object.kind() == StandardWatchEventKinds.OVERFLOW) {
+            System.out.println("Ã©vÃ©nement perdu!");
+        }
+    }
+			
+} catch (IOException e) {
+	
+	e.printStackTrace();
+} catch (InterruptedException e) {
+	
+	e.printStackTrace();
+}
+	}
+	
+	
+	
+public void notifySynchro(){
+		
+	}
+
 
 }
