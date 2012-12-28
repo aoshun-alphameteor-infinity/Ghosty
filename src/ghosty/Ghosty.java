b842bd529668ca89c6ca619e9a7e81c6f63b91b5
karineS <karine.siette@laposte.net>
2012-12-28 10:42:02 +0100

    Update src/ghosty/Ghosty.java

--- src/ghosty/Ghosty.java
+++ src/ghosty/Ghosty.java
@@ -7,7 +7,32 @@ import java.io.OutputStream;
 import java.io.IOException;
 
 public class Ghosty {
+private static boolean debug=false;
 
+private static boolean options() {
+		String s=null;
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNext()){
+			s=sc.next();
+			
+		if(!s.equals("ghosty")) continue;
+			if(sc.hasNext()){ s=sc.next();
+			if(s.equals("--clean")) init();
+			else if(s.equals("--debug")) debug=true;
+			}
+		}
+		return true;
+		
+	}
+	
+		private static void init() {
+		
+		
+	}
+	
+	private static void getInfo(){
+		
+	}
 	
 	
 	public static void main (String args){
