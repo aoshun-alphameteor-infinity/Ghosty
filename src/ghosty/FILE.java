b72943541a2b3dd2186ee1f99a053007d2adfb28
aoshun <aoshun.alphameteor@hotmail.fr>
2012-12-28 21:10:53 +0100

    test

--- src/ghosty/FILE.java
+++ src/ghosty/FILE.java
@@ -22,5 +22,10 @@ public class FILE {
   public int getNumberelt(){
 	  return numberelt;
   }
+  
+  @Override
+  public String toString(){
+	  return location.toString() + " contains " + numberelt;
+  }
  
 }
