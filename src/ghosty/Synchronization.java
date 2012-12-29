18b3c469ccc968597980a5beb1310474b10622ba
aoshun <aoshun.alphameteor@hotmail.fr>
2012-12-29 16:12:37 +0100

    essayons d'upload un fichier

--- src/ghosty/Synchronization.java
+++ src/ghosty/Synchronization.java
@@ -2,13 +2,38 @@ package ghosty;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 
+
 public class Synchronization {
+	
+	public static File uploadFile(Drive host, String extendedfilename, String parentId, Path file){
+		File meta=new File();
+		meta.setTitle(extendedfilename);
+		
+		if(parentId !=null && parentId.length()>0){
+			meta.setParents(Arrays.asList(new File.ParentReference().setId(parentId)));
+		}
+		
+		FileContent content=new FileContent(null,file.toFile());
+		try{
+			File result= host.files().insert(meta,content).execute();
+			return result;
+			}catch (Exception e){
+				System.out.println("error\n"+e.getMessage());
+				return null;
+			}
+		
+	}
+	
 		///pour l'inspiration
 	 /**
 	   * Download a file's content.
